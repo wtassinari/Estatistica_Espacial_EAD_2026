44002ed902c84015366fba05d09e16804ccb02c3
wtassinari <wtassinari@gmail.com>
2026-03-11 17:38:25 -0300

    marco 11 1

--- dados/queimadas/script_extracao_queimadas.r
+++ dados/queimadas/script_extracao_queimadas.r
@@ -31,3 +31,55 @@ dados_geo <- dados |> select(latitude, longitude, frp)
 library(writexl)
 write_xlsx(dados_pontos, "queimadas_pontos.xlsx")
 write_xlsx(dados_geo, "queimadas_geo.xlsx")
+
+
+### Queimadas área
+# Transformar -999 em NA
+
+dados <- read_excel("dados/queimadas/queimadas_full.xlsx")
+
+dados <- dados %>%
+  mutate(
+    numero_dias_sem_chuva = na_if(numero_dias_sem_chuva, -999),
+    precipitacao = na_if(precipitacao, -999),
+    risco_fogo = na_if(risco_fogo, -999),
+    frp = na_if(frp, -999)
+  )
+
+# Agregar por município
+dados <- dados %>%
+  group_by(municipio) %>%
+  summarise(
+    n_queimadas = n(),                          # contagem de linhas
+    media_frp = mean(frp, na.rm = TRUE),        # média FRP
+    precipitacao_total = sum(precipitacao, na.rm = TRUE),
+    precipitacao_media = mean(precipitacao, na.rm = TRUE),
+    media_risco_fogo = mean(risco_fogo, na.rm = TRUE),
+    media_dias_sem_chuva = mean(numero_dias_sem_chuva, na.rm = TRUE)
+  ) %>%
+  mutate(
+    across(where(is.numeric), ~ifelse(is.nan(.x), 0, .x))
+  )
+
+
+library(geobr)
+library(dplyr)
+library(sf)
+library(stringr)
+
+mun_rj <- read_municipality(
+  code_muni = "RJ",
+  year = 2020
+)
+
+mun_rj <- mun_rj %>%
+  mutate(municipio = str_to_upper(name_muni))
+
+queimadas_area <- mun_rj %>%
+  left_join(dados, by = "municipio") |>
+  mutate(across(where(is.numeric), ~ifelse(is.na(.x), 0, .x))) %>%
+  st_drop_geometry()
+
+# Exportar
+library(writexl)
+write_xlsx(queimadas_area, "/home/wagner/Google Drive/UFRRJ/Estatistica_Espacial_EAD_2026/dados/queimadas/queimadas_area.xlsx")
